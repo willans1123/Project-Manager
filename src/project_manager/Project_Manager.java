64476e4fbb7d60cceb248e9f78780e36db319fec
Thiago Rafael Teixeira Nogueira <trthiagorafael@gmail.com>
2016-05-11 17:48:19 -0300

    Preparação do Ambiente Println

--- src/project_manager/Project_Manager.java
+++ src/project_manager/Project_Manager.java
@@ -16,6 +16,10 @@ public class Project_Manager {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Teste");
+                
+                
+                
     }
     
 }
