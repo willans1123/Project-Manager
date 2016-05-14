e68bba8fdc6088afb230c400ca49c0195aec15d3
Thiago Rafael Teixeira Nogueira <trthiagorafael@gmail.com>
2016-05-14 00:23:06 -0300

    Limpando Classe

--- src/project_manager/Project_Manager.java
+++ src/project_manager/Project_Manager.java
@@ -17,10 +17,6 @@ public class Project_Manager {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Escopo Main Principal");
-                
-                      
-                
-                
     }
     
 }
