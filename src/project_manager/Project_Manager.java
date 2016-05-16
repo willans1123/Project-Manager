0f75037e702c6e2af8b00f0a6ac3ddb6b203bc5a
robertraynner <robert_gnt@hotmail.com>
2016-05-16 14:44:30 -0300

    criando as o acesso as camadas do banco de dados

--- src/project_manager/Project_Manager.java
+++ src/project_manager/Project_Manager.java
@@ -17,7 +17,7 @@ public class Project_Manager {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Escopo Main Principal");
-        
+         
                       
                 
                 
