5681ea1c01c39d3cae686a601a5f8f2d4340a869
estefheson <estmateus@gmail.com>
2016-05-16 19:45:22 -0300

    Implementação da classe usuario.java; Criação dos atributos id, login, senha, email e nomeCompleto; Criação dos métodos Getters and Setters;

--- src/Model/java.java
+++ src/Model/java.java
@@ -4,11 +4,83 @@
  * and open the template in the editor.
  */
 package Model;
-
-/**
- *
- * @author W8.1
- */
+//Autor Estefheson
 public class java {
-    
+
+    private int id;
+    private String login;
+    private String senha;
+    private String email;
+    private String nomeCompleto;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nomeCompleto
+     */
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    /**
+     * @param nomeCompleto the nomeCompleto to set
+     */
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
 }
